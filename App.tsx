9503bd18892f3171371e05dc1c0d55437e3cbc64
Erik Slovak <erik.slovak@globallogic.com>
2020-09-25 14:17:44 +0200

    add ApplicationProvider for ui kitten

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
+import { tandemTheme } from './src/theme/custom-theme'
+import { default as mapping } from './src/theme/mapping.json'
 
 import store from './src/redux/store';
 import useCachedResources from './src/utils/hooks/useCachedResources';
@@ -20,8 +24,10 @@ export default function App() {
             <Provider store={store.reduxStore}>
                 <PersistGate loading={null} persistor={store.persistor}>
                     <SafeAreaProvider>
-                        <Navigation colorScheme={colorScheme} />
-                        <StatusBar />
+                        <ApplicationProvider {...eva} theme={tandemTheme} customMapping={mapping}>
+                            <Navigation colorScheme={colorScheme} />
+                            <StatusBar />
+                        </ApplicationProvider>
                     </SafeAreaProvider>
                 </PersistGate>
             </Provider>
